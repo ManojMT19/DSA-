638334a78b5c96d0e6e69dbfbc34fe1e59e000b0
ManojMT19 <mtmanoj19@gmail.com>
2025-10-11 13:09:48 +0530

    < and  <=

---
+++ topics/5-Binary-Search/6-Search-Rotated-II.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool search_rotated_array(vector<int> &arr, int target)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return true;
+
+        if (arr[low] == arr[mid] && arr[mid] == arr[high])
+        {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+        
+        // Left sorted
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= target && target < arr[mid]) //Note < and <= 
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        // Right sorted
+        else
+        {
+            if (arr[mid] < target && target <= arr[high]) //Note < and <= 
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    vector<int> nums{5,6,7,8,1,2,3,4,5,5,5};
+    
+    cout << search_rotated_array(nums , 8);
+
+    return 0;
+}
