2a01ecba2a6f25f895a860684d57df4f11545bed
ManojMT19 <mtmanoj19@gmail.com>
2025-10-17 18:49:27 +0530

    changed

--- topics/5-Binary-Search/BS-on-Answers/smallest-divisor.cpp
+++ topics/5-Binary-Search/BS-on-Answers/smallest-divisor.cpp
@@ -1,30 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-int divisor(vector<int> &nums, int threshold)
+int smallest_divisor_brute(vector<int> &nums, int threshold)
 {
     int n = nums.size();
-    for (int i = 0; i < n; i++)
-    {
-        vector<int> temp;
-        int j = 1;
-        temp.push_back(nums[i] / j);
-        int count = 0;
-        
-        for(int x : temp)
-            count += temp[x];
+    int m = *max_element(nums.begin(), nums.end());
 
-        if (count < threshold)
+    for (int i = 1; i <= m; i++)
+    {
+        int sum = 0;
+        for (int x : nums)
         {
-            return j;
+            sum += (x + i - 1) / i; // ceil division
         }
-        else j++;
+        if (sum <= threshold)
+            return i; // found smallest divisor
     }
     return -1;
 }
+int smallest_divisor_optimised(vector<int> &nums, int threshold)
+{
+    int n = nums.size();
+    int m = *max_element(nums.begin(), nums.end());
+    int low = 1, high = m, ans = -1;
+
+    if (n > threshold)
+        return -1; // this is to check if their is no divisor
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += (nums[i] + mid - 1) / mid; // ceil division
+            // ceil(a / b) = (a + b - 1) / b
+            // (5 + 2 - 1) / 2 = 6 / 2 = 3   ✅ ceil(5/2)
+            // (10 + 3 - 1) / 3 = 12 / 3 = 4 ✅ ceil(10/3)
+        }
+        if (sum <= threshold)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 int main()
 {
-    vector<int> arr{1,2,5,9};
-    cout << divisor(arr , 6);
+    vector<int> arr{1, 2, 5, 9};
+    cout << smallest_divisor_optimised(arr, 6);
 
     return 0;
 }
