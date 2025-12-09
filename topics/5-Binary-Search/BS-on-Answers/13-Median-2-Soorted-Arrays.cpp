cd06fba97dd0973327fa00c00d39b82d51485538
ManojMT19 <mtmanoj19@gmail.com>
2025-12-09 10:38:57 +0530

    TC n SC

--- topics/5-Binary-Search/BS-on-Answers/13-Median-2-Soorted-Arrays.cpp
+++ topics/5-Binary-Search/BS-on-Answers/13-Median-2-Soorted-Arrays.cpp
@@ -1,33 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-double median_2_sorted(vector<int> &arr1 ,vector<int>&arr2)
+double median_2_sorted_brute(vector<int> &arr1 ,vector<int>&arr2)
 {
     vector<int> temp(arr1.begin(), arr1.end());
     temp.insert(temp.end(),arr2.begin(),arr2.end());
     sort(temp.begin(), temp.end());
     int n = temp.size();
-    int z = -1;
     double ans = 0;
     if (n % 2 == 0)
     {
-        double a1 = n/2;
-        double b1 = a1 + 1;
-        double c1 = (a1 + b1)/2;
-        ans = temp[c1];
+        ans = ((double)temp[n/2] + (double)temp[n/2-1])/2.0;
     }
     else
     {
-        double a1 = n/2;
-        ans = temp[a1];
+        ans = temp[n/2];
     }
     return ans;
+    /*
+    ✅ Time Complexity (TC)
+    Merging into temp → O(N)
+    Sorting temp → O(N log N)
+    Finding median → O(1)
+    👉 Final TC = O(N log N)
+
+    ✅ Space Complexity (SC)
+    Extra array temp of size N → O(N)
+    Recursive stack from sort() → O(log N)
+    👉 Final SC = O(N) (log N stack is negligible)
+    */
 }
+
 int main()
 {
-    vector<int>nums1{1,3};
-    vector<int>nums2{2,4};
+    vector<int>nums1{1,4};
+    vector<int>nums2{2,6};
 
-    cout << median_2_sorted(nums1,nums2);
+    cout << median_2_sorted_brute(nums1,nums2);
 
     
     return 0;
