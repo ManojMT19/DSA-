0bb03695f7a5f622e690ae24e18b908bd576d473
ManojMT19 <mtmanoj19@gmail.com>
2025-12-23 23:01:33 +0530

    time

---
+++ topics/6-Strings/102-Max-Nesting-Depth-Parenthesis.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxDepth(string s) // Leetcode 1614
+{
+    int n = s.size();
+    int count = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(s[i] == '(')count++;
+        else if(s[i] == ')')count--;
+        ans = max(ans,count);
+    }
+    return ans;
+}
+int main()
+{
+    // string s = "(1)+((2))+(((3)))";
+    // string s = "(1+(2*3)+((8)/4))+1";
+    string s = "()(())((()()))";
+    cout << maxDepth(s);
+
+    return 0;
+}
