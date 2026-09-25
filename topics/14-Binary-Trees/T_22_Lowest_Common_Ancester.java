068c2bc15879bb4b59c77e605460bf09dd6b23a0
ManojMT19 <mtmanoj19@gmail.com>
2026-09-25 07:49:26 +0530

    Belagge pushing

--- topics/14-Binary-Trees/T_22_Lowest_Common_Ancester.java
+++ topics/14-Binary-Trees/T_22_Lowest_Common_Ancester.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class TreeNode
 {
     int data;
@@ -21,15 +24,17 @@ class TreeNode
     }
 }
 
-public class T_22_Lowest_Common_Ancester  // Leetcode 236
+public class T_22_Lowest_Common_Ancester // Leetcode 236
 {
-    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)  // worst problem didnt understand
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) // worst problem didnt understand
     {
-        // if 1 n 2 both r ancesters of p n q we should select 2 bcz we should select the lowest or deepest ancesters  
+        // if 1 n 2 both r ancesters of p n q we should select 2 bcz we should select
+        // the lowest or deepest ancesters
 
-        if(root == null)return root;
+        if (root == null)
+            return root;
 
-        if(root == p || root == q)
+        if (root == p || root == q)
         {
             return root;
         }
@@ -37,18 +42,67 @@ public class T_22_Lowest_Common_Ancester  // Leetcode 236
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if(left == null)
+        if (left == null)
         {
             return right;
-        }
-        else if(right == null)
+        } else if (right == null)
         {
             return left;
-        }
-        else
+        } else
         {
             return root;
         }
 
+    }
+
+    // -------------------------------------------------------------------------------------------------------------------------
+
+    public static boolean findPath(TreeNode root, TreeNode target, List<TreeNode> path)
+    {
+        if (root == null)
+            return false;
+
+        path.add(root);
+
+        if (root == target)
+            return true;
+
+        if (root.left != null)
+        {
+            if (findPath(root.left, target, path))
+                return true;
+
+            path.remove(path.size() - 1); // backtracking
+        }
+
+        if (root.right != null)
+        {
+            if (findPath(root.right, target, path))
+                return true;
+
+            path.remove(path.size() - 1); // backtracking
+        }
+
+        return false;
+    }
+
+    public static TreeNode lowestCommonAncestor_brute(TreeNode root, TreeNode p, TreeNode q)
+    {
+
+        List<TreeNode> pathP = new ArrayList<>();
+        List<TreeNode> pathQ = new ArrayList<>();
+
+        findPath(root, p, pathP);
+        findPath(root, q, pathQ);
+
+        int i = 0;
+
+        while (i < pathP.size() && i < pathQ.size() && pathP.get(i) == pathQ.get(i))
+        {
+
+            i++;
+        }
+
+        return pathP.get(i - 1);
     }
 }
